3889a8df50c3a327876c2d2fef8dbdb6e19a77ed
khunsha123 <aynafarooq70@gmail.com>
2024-06-03 17:42:24 +0530

    Changes

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from keras import backend as K
 K.set_image_data_format('channels_first')
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
+
 from facerecog_utils import *
 from inception_blocks import *
 from database_utils import *
